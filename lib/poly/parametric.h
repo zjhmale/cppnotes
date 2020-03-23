b79bfb5bafd1a5cc056273e64a0bd42a780fe111
zjhsdtc <zjhsdtc@gmail.com>
2020-03-23 17:35:58 +0800

    chore: typename in template

--- lib/poly/parametric.h
+++ lib/poly/parametric.h
@@ -6,7 +6,7 @@ using namespace std;
 namespace poly::parametric {
     // Parametric is also called compile-time polymorphism
     // template<class T>
-    template<class T>
+    template<typename T>
     T pmax(T a, T b) {
         return a > b ? a : b;
     }
